5a33323cd131a87625ff7cba55dc9cd6aea8ee8b
Your Name <you@example.com>
2024-05-14 20:08:05 +0900

    とりあえずコミット

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <iostream>
+
+int main(int argc,char **argv)
+{
+	if(argc != 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+		stransform (argv[1].begin(), argv[1].end(), argv[1].begin(), toupper);
+}
+
