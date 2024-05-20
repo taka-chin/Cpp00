9b3a35abd605538169d3bb55b52f3879d7e39efa
Your Name <you@example.com>
2024-05-20 21:28:55 +0900

    とりあえず動いた

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,6 +5,10 @@ int main(int argc,char **argv)
 {
 	if(argc != 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	/* else */
-	/* 	stransform (argv[1].begin(), argv[1].end(), argv[1].begin(), toupper); */
+	else
+	{
+		std::string message = argv[1];
+		std::transform(message.cbegin(), message.cend(), message.begin(), toupper);
+		std::cout << message << "\n";
+	}
 }
