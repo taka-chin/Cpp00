62c2395ced5836177cf7b24e1a13f08b2a3bd9ba
Your Name <you@example.com>
2024-05-21 19:28:50 +0900

    fin

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,12 +3,21 @@
 
 int main(int argc,char **argv)
 {
-	if(argc != 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else
+	int i;
+
+	i = 1;
+	if(argc == 1)
 	{
-		std::string message = argv[1];
-		std::transform(message.cbegin(), message.cend(), message.begin(), toupper);
-		std::cout << message << "\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return(0);
 	}
+	while(i < argc)
+	{
+			std::string message = argv[i];
+			std::transform(message.begin(), message.end(), message.begin(), toupper);
+			std::cout << message ;
+			i++;
+	}
+	std::cout << "\n" ;
+	return(0);
 }
