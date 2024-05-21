a68a8e92416304745cf9e00c04970d73f5daa8e0
Your Name <you@example.com>
2024-05-21 21:18:40 +0900

    std::cinで分岐

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
+#include <string>
 
 /* It has an array of contacts. */
-/* It can store a maximum of 8 contacts. If the user tries to add a 9th contact, */
+/* It can store a maximum of 8 contacts. If the user tries to add a 9th contact,
+ */
 /* replace the oldest one by the new one. */
 /* Please note that dynamic allocation is forbidden. */
 
-class PhoneBook
-{
-	private:
-		Contact contacts[8];
+/* class PhoneBook */
+/* { */
+/* 	private: */
+/* 		Contact contacts[8]; */
+/* } */
+
+/* /1* Stands for a phonebook contact. *1/ */
+/* class Contact */
+/* { */
+/* 	private: */
+/* 		std::string name; */
+/* 		std::string phoneNumber; */
+/* 		std::string email; */
+/* 		std::string address; */
+/* } */
+
+int main() {
+  std::string command;
+
+  while (true) {
+    std::cin >> command;
+    if (command == "ADD")
+      std::cout << "ADD\n";
+    if (command == "SERACH")
+      std::cout << "SERACH\n";
+    if (command == "EXIT")
+      break;
+  }
+  return (0);
 }
-
-
-/* Stands for a phonebook contact. */
-class Contact
-{
-	private:
-		std::string name;
-		std::string phoneNumber;
-		std::string email;
-		std::string address;
-}
-
-
