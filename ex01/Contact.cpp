0b5167b0064781b162c72b7a3cebc81ed790ef0b
Your Name <you@example.com>
2024-05-29 19:52:31 +0900

    [add] Contact Class

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,2 +1,25 @@
-  void getContact(Contact *contact);
-  void setContact(Contact *contact);
+#include "Contact.hpp"
+
+const std::string &Contact::getFirstName() { return first_name_; }
+
+const std::string &Contact::getLastName() { return last_name_; }
+
+const std::string &Contact::getNickName() { return nick_name_; }
+
+const std::string &Contact::getPhoneNumber() { return phone_number_; }
+
+const std::string &Contact::getDarkestSecret() { return darkest_secret_; }
+
+void Contact::setFirstName(std::string first_name) { first_name_ = first_name; }
+
+void Contact::setLastName(std::string last_name) { last_name_ = last_name; }
+
+void Contact::setNickName(std::string nick_name) { nick_name_ = nick_name; }
+
+void Contact::setPhoneNumber(std::string phone_number) {
+  phone_number_ = phone_number;
+}
+
+void Contact::setDarkestSecret(std::string darkest_secret) {
+  darkest_secret_ = darkest_secret;
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,27 @@
+#ifndef CPP00_EX01_Contact_H_
+#define CPP00_EX01_Contact_H_
+
+#include <string>
+
+class Contact {
+private:
+  std::string first_name_;
+  std::string last_name_;
+  std::string nick_name_;
+  std::string phone_number_;
+  std::string darkest_secret_;
+
+public:
+  const std::string &getFirstName();
+  const std::string &getLastName();
+  const std::string &getNickName();
+  const std::string &getPhoneNumber();
+  const std::string &getDarkestSecret();
+  void setFirstName(std::string first_name);
+  void setLastName(std::string last_name);
+  void setNickName(std::string nick_name);
+  void setPhoneNumber(std::string phone_number);
+  void setDarkestSecret(std::string darkest_secret);
+};
+
+#endif
